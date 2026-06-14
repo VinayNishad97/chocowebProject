7d4b2a7a7f4072039312c593a802f893b2c11ce5
VinayNishad97 <vinay972591@gmail.com>
2026-06-14 14:46:18 +0530

    added shadcn Ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-    return <h1>Welcome to SQL-Agent</h1>;
+    return (
+        <>
+            <h1>Welcome to SQL-Agent</h1>
+            <Button className="w-25">Click Me</Button>
+        </>
+    );
 }
