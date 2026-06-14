2273d8cfe49246665caba5da18bdfa3e97c2181e
VinayNishad97 <vinay972591@gmail.com>
2026-06-14 11:07:37 +0530

    get invontory

--- src/app/api/inventories/route.ts
+++ src/app/api/inventories/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/src/lib/db/db";
-import { inventories } from "@/src/lib/db/schema";
+import { inventories, products, warehouses } from "@/src/lib/db/schema";
 import { inventorySchema } from "@/src/lib/validators/inventorySchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request: Request) {
     const req = await request.json();
@@ -15,6 +16,25 @@ export async function POST(request: Request) {
     try {
         await db.insert(inventories).values(velidatedData);
         return Response.json({ mssg: "OK" }, { status: 201 });
+    } catch (error) {
+        return Response.json({ mssg: error }, { status: 500 });
+    }
+}
+
+export async function GET() {
+    try {
+        const allinvontories = await db
+            .select({
+                id: inventories.id,
+                sku: inventories.sku,
+                warehouse: warehouses.name,
+                product: products.name,
+            })
+            .from(inventories)
+            .leftJoin(warehouses, eq(inventories.warehouseId, warehouses.id))
+            .leftJoin(products, eq(inventories.productId, products.id))
+            .orderBy(desc(inventories.id));
+        return Response.json(allinvontories, { status: 200 });
     } catch (error) {
         return Response.json({ mssg: error }, { status: 500 });
     }
