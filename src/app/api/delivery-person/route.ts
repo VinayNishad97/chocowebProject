f4d37bd9a3497c6d453accca4a8f9d1fa86278c6
VinayNishad97 <vinay972591@gmail.com>
2026-06-14 09:39:16 +0530

    get all devlivery persons

--- src/app/api/delivery-person/route.ts
+++ src/app/api/delivery-person/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/src/lib/db/db";
-import { deliveryPersons } from "@/src/lib/db/schema";
+import { deliveryPersons, warehouses } from "@/src/lib/db/schema";
 import { deliveryPersonSchema } from "@/src/lib/validators/deliveryPersonSchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(reqest: Request) {
     const reqData = await reqest.json();
@@ -16,5 +17,30 @@ export async function POST(reqest: Request) {
         return Response.json({ mssg: "OK" }, { status: 201 });
     } catch (err) {
         return Response.json({ mssg: err }, { status: 500 });
+    }
+}
+
+export async function GET() {
+    try {
+        const data = await db
+            .select({
+                id: deliveryPersons.id,
+                name: deliveryPersons.name,
+                phone: deliveryPersons.phone,
+                warehouse: warehouses.name,
+            })
+            .from(deliveryPersons)
+            .leftJoin(
+                warehouses,
+                eq(deliveryPersons.warehouseId, warehouses.id),
+            )
+            .orderBy(desc(deliveryPersons.id));
+
+        return Response.json(data, { status: 200 });
+    } catch (error) {
+        return Response.json(
+            { mssg: "failed to fetch delivery person" },
+            { status: 500 },
+        );
     }
 }
