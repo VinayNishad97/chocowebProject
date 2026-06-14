0431190b76e3ed3e74f718571db21e21e65b848e
VinayNishad97 <vinay972591@gmail.com>
2026-06-14 17:20:02 +0530

    some changes in dashboard ui

--- components/team-switcher.tsx
+++ components/team-switcher.tsx
@@ -1,27 +1,59 @@
 "use client";
+
 import Image from "next/image";
 import {
-    SidebarMenu,
-    SidebarMenuItem,
-    useSidebar,
-} from "@/components/ui/sidebar";
+    DropdownMenu,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SidebarMenu, SidebarMenuItem } from "@/components/ui/sidebar";
 
 export function TeamSwitcher() {
-    const { isMobile } = useSidebar();
-
     return (
         <SidebarMenu>
             <SidebarMenuItem>
-                <div className="flex gap-2 ">
-                    <Image
-                        src={"/choco.png"}
-                        width={50}
-                        height={20}
-                        alt="chocolatee"
-                    />
-                    <h1 className="text-center text-4xl">Choco</h1>
-                </div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <div className="flex gap-2 ">
+                            <Image
+                                src={"/choco.png"}
+                                width={50}
+                                height={20}
+                                alt="chocolatee"
+                            />
+                            {/* <h1 className="text-center text-4xl">Choco</h1>{" "} */}
+                        </div>
+                    </DropdownMenuTrigger>
+                </DropdownMenu>
             </SidebarMenuItem>
         </SidebarMenu>
     );
 }
+
+// ("use client");
+// import Image from "next/image";
+// import {
+//     SidebarMenu,
+//     SidebarMenuItem,
+//     useSidebar,
+// } from "@/components/ui/sidebar";
+// export function TeamSwitcher() {
+//     const { isMobile } = useSidebar();
+//     return (
+//         <SidebarMenu>
+//             {" "}
+//             <SidebarMenuItem>
+//                 {" "}
+//                 <div className="flex gap-2 ">
+//                     {" "}
+//                     <Image
+//                         src={"/choco.png"}
+//                         width={50}
+//                         height={20}
+//                         alt="chocolatee"
+//                     />{" "}
+//                     <h1 className="text-center text-4xl">Choco</h1>{" "}
+//                 </div>{" "}
+//             </SidebarMenuItem>{" "}
+//         </SidebarMenu>
+//     );
+// }
