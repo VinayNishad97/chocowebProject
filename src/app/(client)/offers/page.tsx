80c974b7105eaa42fac8f06974705bccdad04b0f
VinayNishad97 <vinay972591@gmail.com>
2026-06-30 18:53:04 +0530

    fixed offers page

--- src/app/(client)/offers/page.tsx
+++ src/app/(client)/offers/page.tsx
@@ -7,7 +7,7 @@ export default function Offers() {
             <Offer />
             <Nav />
             <div className="h-screen w-full bg-amber-200 flex flex-col justify-center items-center">
-                <div>
+                <div className="flex flex-col justify-center items-center">
                     <h1 className="text-6xl text-amber-600">NO OFFERS !</h1>
                     <h1 className="text-3xl text-amber-500">
                         No offers for regular customer's yet. check again after
